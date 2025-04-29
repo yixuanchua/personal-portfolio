0ddd75a33c3c316c4123cdbacaf310db48b5c6da
yixuanchua <166963686+yixuanchua@users.noreply.github.com>
2025-04-29 16:47:01 +0800

    Added card components for projects section

---
+++ src/components/card.jsx
@@ -0,0 +1,28 @@
+import Tag from "./tag";
+
+const Card = ( { title, description, image1, image2, imageAlt, technologies } ) => {
+    return (
+        <>
+            <div className="flex flex-col relative overflow-hidden shadow-lg bg-neutral-200 h-60 w-[90%] max-w-125 rounded-lg group">
+                <img src={image1} alt={imageAlt} className="w-full h-full object-cover group-hover:scale-110 transition duration-500"/>
+                {image2 ? 
+                    <img src={image2} alt={imageAlt} className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-110 opacity-0 group-hover:opacity-100 transition duration-500"/>
+                    : <div className="hidden"></div>
+                }
+                <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent opacity-40 group-hover:opacity-100 transition duration-300"></div>
+                <h3 className="text-lg text-white font-semibold group-hover:-translate-y-24 group-hover:opacity-0 transition-all duration-300 absolute bottom-0 mb-2 text-center w-full">{title}</h3>
+                <div className="text-white mt-auto text-center w-full absolute bottom-0 translate-y-full group-hover:translate-y-0 transition duration-300 ease-in-out">
+                    <h3 className="text-lg font-semibold mx-4">{title}</h3>
+                    <h6 className="text-xs text-wrap my-2 mx-4">{description}</h6>
+                    <div className="flex gap-4 justify-center mb-4 overflow-x-scroll">
+                        {technologies.map((tech) => (
+                            <Tag key={tech} text={tech}/>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Card
---
+++ src/components/tag.jsx
@@ -0,0 +1,9 @@
+const Tag = ({ text }) => {
+    return (
+        <>
+            <p className="text-xs text-teal-100 text-nowrap bg-teal-200/60 px-2 py-0.5 rounded-full">{text}</p>
+        </>
+    );
+}
+
+export default Tag
