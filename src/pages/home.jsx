66d5b67eeb66a6f823a20a86423ede9ff0def67f
yixuanchua <166963686+yixuanchua@users.noreply.github.com>
2025-05-01 05:14:49 +0800

    Updated homepage design

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,18 @@
 const Home = () => {
     return (
         <>
-        <div id="home" className='h-screen place-content-center mx-4'>
-            <h1 className='flex justify-center font-bold text-emerald-500 p-3 text-5xl'>
-            Hi, i'm Chua Yi Xuan
+        <div id="home" className='z-10 h-screen place-content-center mx-4'>
+            <h1 className='flex flex-col sm:flex-row justify-center font-aileron font-bold text-white hover:text-rose-500/90 transition-all duration-300 animate-scalePulse py-3 px-6 sm:text-5xl text-4xl'>
+                <span>Hi,&nbsp;</span>
+                <span>I'm Chua Yi Xuan</span>
             </h1>
-            <h2 className="flex justify-center text-white mx-4">
+            <h2 className="font-sharetechmono flex justify-center text-white mx-4">
                 A web developer. Someone who wants to make the world a better place.
             </h2>
             <div className='flex justify-center'>
-                <button className="mt-8 px-3 py-2 rounded-full text-white border border-white hover:border-emerald-500 hover:text-emerald-500">My experience</button>
+                <a href="#experience" className="font-sharetechmono mt-8 px-3 py-2 text-white border border-white hover:border-rose-500 hover:text-rose-500">
+                    My experience
+                </a>
             </div>
         </div>
         </>
